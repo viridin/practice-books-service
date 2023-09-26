df2d0c96d247e5477c7579885c2ea11cbc8c4e10
odmen <dima.nishchemenko@gmail.com>
2023-09-26 13:44:51 +0300

    add swagger documentation

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Post, Query, Body, ParseIntPipe } from '@nestjs/common';
+import { ApiTags, ApiResponse, ApiQuery } from '@nestjs/swagger';
 import { Book } from './book.model';
 
+@ApiTags('Books')
 @Controller('books')
 export class BooksController {
   private books: Book[] = [];
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Returns a list of books' })
+  @ApiQuery({ name: 'limit', type: Number, required: false })
+  @ApiQuery({ name: 'start', type: Number, required: false })
   getBooks(
     @Query('limit', ParseIntPipe) limit = 20,
     @Query('start', ParseIntPipe) start = 0,
@@ -16,6 +21,7 @@ export class BooksController {
   }
 
   @Post()
+  @ApiResponse({ status: 201, description: 'Creates a new book' })
   createBook(@Body() bookData: { title: string; author: string; year: number }): Book {
     const id = this.books.length + 1;
     const book = new Book(id, bookData.title, bookData.author, bookData.year);
