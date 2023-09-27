94a73b05ae79dab7c7d6b77fee26d863cdf3b882
odmen <dima.nishchemenko@gmail.com>
2023-09-27 23:13:59 +0300

    add postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+//import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule,
+    /*TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: '',
+      password: '',
+      database: '',
+      entities: [],
+      autoLoadEntities: true,
+    }),*/],
   controllers: [AppController],
   providers: [AppService],
 })
